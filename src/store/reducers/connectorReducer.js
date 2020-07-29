1f8972f904f246a9c88e46bc21afcbb37a1b29b3
Christian Aichner <contact@aichner-christian.com>
2020-07-29 18:04:54 +0200

    Implement connector reducer
    The reducer has been implemented.

---
+++ src/store/reducers/connectorReducer.js
@@ -0,0 +1,100 @@
+// Have initial state for when state is not ready to be passed
+const initState = {
+  connectors: [],
+  selectedConnector: null,
+  testConnector: null,
+  error: null,
+};
+
+const connectorReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "GET_CONNECTORS_SUCCESS":
+      return {
+        ...state,
+        connectors: action.payload.data ? action.payload.data : [],
+        selectedConnector: null,
+        testConnector: null,
+        error: null,
+      };
+    case "GET_CONNECTORS_FAIL":
+      console.error(
+        action.payload.error.code,
+        action.payload.error.origin,
+        action.payload.error.msg
+      );
+
+      return {
+        ...state,
+        connectors: [],
+        selectedConnector: null,
+        testConnector: null,
+        error: action.payload.error,
+      };
+    case "GET_CONNECTOR_BY_ID_SUCCESS":
+      return {
+        ...state,
+        selectedConnector: action.payload.data,
+        testConnector: null,
+        error: null,
+      };
+    case "GET_CONNECTOR_BY_ID_FAIL":
+      console.error(
+        action.payload.error.code,
+        action.payload.error.origin,
+        action.payload.error.msg
+      );
+
+      return {
+        ...state,
+        selectedConnector: false,
+        testConnector: null,
+        error: action.payload.error,
+      };
+    case "TEST_CONNECTOR_SUCCESS":
+      return {
+        ...state,
+        testConnector: true,
+      };
+    case "TEST_CONNECTOR_FAIL":
+      console.error(
+        action.payload.error.code,
+        action.payload.error.origin,
+        action.payload.error.msg
+      );
+
+      return {
+        ...state,
+        testConnector: false,
+        error: action.payload.error,
+      };
+    case "CREATE_CONNECTOR_SUCCESS":
+      return {
+        ...state,
+        selectedConnector: false,
+        testConnector: null,
+        connectors: action.payload.data,
+      };
+    case "CREATE_CONNECTOR_FAIL":
+      console.error(
+        action.payload.error.code,
+        action.payload.error.origin,
+        action.payload.error.msg
+      );
+
+      return {
+        ...state,
+        selectedConnector: false,
+        testConnector: null,
+        error: action.payload.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export default connectorReducer;
+
+/**
+ * SPDX-License-Identifier: (EUPL-1.2)
+ * Copyright © 2020 Simon Prast
+ */
