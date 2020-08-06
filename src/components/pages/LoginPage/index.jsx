8d1307f1d7eaa95eddd88a63ba1cf5edb6a8781c
Christian Aichner <contact@aichner-christian.com>
2020-08-06 10:49:46 +0200

    Add PropTypes and input fields
    The proptypes have been introduced.

--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -2,6 +2,8 @@
 //> React
 // Contains all the functionality necessary to define React components
 import React from "react";
+// Prop types
+import PropTypes from "prop-types";
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
@@ -35,7 +37,10 @@ import logoImg from "../../../assets/navigation/logo.png";
 /** @class The Admin parent page component which will include all Admin pages */
 class LoginPage extends React.Component {
   state = { email: "", password: "" };
+
   render() {
+    const { passwordAuth } = this.props;
+
     return (
       <MDBContainer className="my-5 h-100">
         <MDBRow className="flex-center h-100">
@@ -43,29 +48,49 @@ class LoginPage extends React.Component {
             <MDBCard className="border">
               <MDBCardBody className="text-center">
                 <img src={logoImg} alt="SNEK OPS Logo" className="mb-2" />
-                <p className="lead">Login to OPS</p>
-                <MDBInput
-                  outline
-                  name="email"
-                  type="text"
-                  label="E-Mail"
-                  value={this.state.email}
-                  onChange={(e) =>
-                    this.setState({ [e.target.name]: e.target.value })
-                  }
-                />
-                <MDBInput
-                  outline
-                  name="password"
-                  type="password"
-                  label="Password"
-                  value={this.state.password}
-                  onChange={(e) =>
-                    this.setState({ [e.target.name]: e.target.value })
-                  }
-                  containerClass="mb-2"
-                />
-                <MDBBtn color="indigo">Login</MDBBtn>
+                {passwordAuth ? (
+                  <>
+                    <p className="lead">Please enter your password</p>
+                    <MDBInput
+                      outline
+                      name="password"
+                      type="password"
+                      label="Password"
+                      value={this.state.password}
+                      onChange={(e) =>
+                        this.setState({ [e.target.name]: e.target.value })
+                      }
+                      containerClass="mb-2"
+                    />
+                    <MDBBtn color="indigo">Authenticate</MDBBtn>
+                  </>
+                ) : (
+                  <>
+                    <p className="lead">Login to OPS</p>
+                    <MDBInput
+                      outline
+                      name="email"
+                      type="text"
+                      label="E-Mail"
+                      value={this.state.email}
+                      onChange={(e) =>
+                        this.setState({ [e.target.name]: e.target.value })
+                      }
+                    />
+                    <MDBInput
+                      outline
+                      name="password"
+                      type="password"
+                      label="Password"
+                      value={this.state.password}
+                      onChange={(e) =>
+                        this.setState({ [e.target.name]: e.target.value })
+                      }
+                      containerClass="mb-2"
+                    />
+                    <MDBBtn color="indigo">Login</MDBBtn>
+                  </>
+                )}
               </MDBCardBody>
             </MDBCard>
           </MDBCol>
@@ -74,6 +99,16 @@ class LoginPage extends React.Component {
     );
   }
 }
+//#endregion
+
+//#region > Prop Types
+LoginPage.propTypes = {
+  passwordAuth: PropTypes.bool,
+};
+
+LoginPage.defaultProps = {
+  passwordAuth: true,
+};
 //#endregion
 
 //#region > Exports
